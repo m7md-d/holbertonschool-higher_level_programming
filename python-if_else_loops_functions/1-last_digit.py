1ab33f6bf190325bedaa6550b2aa3b7049593378
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-12 13:07:50 +0300

    Fix spacing in conditional statements for consistency

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if(number < 0):
+if (number < 0):
     last_digit = number * -1
 else:
     last_digit = number
 last_digit = last_digit % 10
-if(last_digit == 0):
+if (last_digit == 0):
     tld = "and is 0"
-elif(last_digit > 5):
+elif (last_digit > 5):
     tld = "and is greater than 5"
 else:
     tld = "and is less than 6 and not 0"
