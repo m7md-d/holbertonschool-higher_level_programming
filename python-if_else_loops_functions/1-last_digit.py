c699b4acfb9dd55c740ab8480c40a660d6b965bd
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-12 13:10:58 +0300

    Fix indentation and improve last digit calculation logic

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 if (number < 0):
-    last_digit = number * -1
+	last_digit = number * -1
+	last_digit = last_digit % 10
+	last_digit = last_digit * -1
 else:
-    last_digit = number
-last_digit = last_digit % 10
+	last_digit = number % 10
 if (last_digit == 0):
-    tld = "and is 0"
+	tld = "and is 0"
 elif (last_digit > 5):
-    tld = "and is greater than 5"
+	tld = "and is greater than 5"
 else:
-    tld = "and is less than 6 and not 0"
+	tld = "and is less than 6 and not 0"
 print(f"Last digit of {number} is {last_digit} {tld}")
