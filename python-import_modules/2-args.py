2bcbb47ce501df4658cf839dcf5a0fdff9caf8f2
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-14 11:10:33 +0300

    Refactor 2-args.py to include main guard for script execution

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
-ln = len(argv)
-if ln == 1:
-    print("0 arguments.")
-elif ln == 2:
-    print(f"1 argument:\n1: {argv[1]}")
-else:
-    print(f"{ln - 1} arguments:")
-    for i in range(1, ln):
-        print(f"{i}: {argv[i]}")
+if __name__ == "__main__":
+    ln = len(argv)
+    if ln == 1:
+        print("0 arguments.")
+    elif ln == 2:
+        print(f"1 argument:\n1: {argv[1]}")
+    else:
+        print(f"{ln - 1} arguments:")
+        for i in range(1, ln):
+            print(f"{i}: {argv[i]}")
