94bb4da69940b3499bd44d21a53371e7b5e781db
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-19 11:41:28 +0300

    Add the 2th code.

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new = []
+    total = 0
+    for i in my_list:
+        if i in new:
+            continue
+        total += i
+        new.append(i)
+    return total
