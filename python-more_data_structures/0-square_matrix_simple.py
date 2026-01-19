396bf0cf09a28d922b70048ef3e0bd65b3cfa94e
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-19 10:25:34 +0300

    Add the first code.

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+def square_matrix_simple(matrix=[]):
+    new = []
+    for i in matrix:
+        new_row = []
+        for j in i:
+            new_row.append(j * j)
+        new.append(new_row)
+    return new
