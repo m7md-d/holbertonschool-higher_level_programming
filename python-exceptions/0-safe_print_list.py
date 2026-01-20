c0c7730b963b0b0d7099a4914ab3b64ac82a345a
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-20 13:55:36 +0300

    Add the first code.

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     for i in my_list:
         if j >= x:
             break
-        new.append(i)
+        print(i, end="")
         j += 1
-    print(new)
+    print()
     return j
