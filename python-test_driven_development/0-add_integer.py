4bcd7374ee0f23981e9df51fd75533e0a5b227e5
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-23 13:55:31 +0300

    Add documentes to the first code.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+
+"""
+This module provides a function to add two integers.
+It performs type checking and casting for floats to integers.
+The results are returned as the sum of the two numbers.
+"""
 def add_integer(a, b=98):
+    """
+    Adds two integers. Floats are casted to integers.
+    Raises TypeError if a or b are not integers or floats.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
