3b9c7dccf25271fec9f5f07129366aeff7086791
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-23 14:04:52 +0300

    Fix the documentes and handle Inf,NaN.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-
 """
 This module provides a function to add two integers.
 It performs type checking and casting for floats to integers.
 The results are returned as the sum of the two numbers.
 """
+
 def add_integer(a, b=98):
     """
-    Adds two integers. Floats are casted to integers.
-    Raises TypeError if a or b are not integers or floats.
+    Adds two integers.
+    Args:
+        a: first number (int or float)
+        b: second number (int or float), defaults to 98
+    Returns:
+        The addition of a and b as an integer.
+    Raises:
+        TypeError: If a or b are not integers, floats, or are Inf/NaN.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    if a != a or a == float('inf') or a == float('-inf'):
+        raise TypeError("a must be an integer")
+    if b != b or b == float('inf') or b == float('-inf'):
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
