78d8e294e3e1c092d6e167b7db9616f3841b5dcd
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-26 19:48:44 +0300

    Fix the 6th code.

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -18,8 +18,8 @@ class Square:
             size (int): The size of the new square.
             position (tuple): The position of the new square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -54,7 +54,7 @@ class Square:
         if (not isinstance(value, tuple) or
             len(value) != 2 or
             not all(isinstance(num, int) for num in value) or
-            not all(num < 0 for num in value)):
+            not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
