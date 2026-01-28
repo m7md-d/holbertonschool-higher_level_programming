7d19757bd6ce4146c53624a56ad53c14b7c4f19e
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-28 13:17:15 +0300

    Fix the 1th code.

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
         """
         Initialize a new Rectangle instance.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
