cfb8eacca7b7e1dbd536258c5a71e6639b30db31
Dark-Ghost1 <wazrahm@gmail.com>
2026-01-28 19:00:52 +0300

    Add the 9th code.

---
+++ python-more_classes/9-rectangle.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+"""
+Simple rectangle!
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class
+    """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialize a new Rectangle instance.
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """
+        width set
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        width setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        height set
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        height setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        return the rectangle area
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """
+        return the rectangle perimeter
+        """
+        if (self.__height == 0 or self.__width == 0):
+            return 0
+        return (self.__height + self.__width) * 2
+
+    def __str__(self):
+        """
+        return string of the rectangle with the character #
+        """
+        if (self.__height == 0 or self.__width == 0):
+            return ""
+        moraba3 = ""
+        c = ""
+        for i in range(self.__height):
+            moraba3 += c
+            moraba3 += (str(self.print_symbol) * self.__width)
+            c = "\n"
+        return moraba3
+
+    def __repr__(self):
+        """
+        return a string representation of the rectangle.
+        """
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """
+        Print a message when an instance of Rectangle is deleted.
+        """
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        Static method that returns the biggest rectangle based on the area
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        return rect_1
+
+    @classmethod
+    def square(cls, size=0):
+        """
+        returns new Rectangle instance with width == height == size
+        """
+        return cls(size, size)
