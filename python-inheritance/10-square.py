51225c2aca3ea836ded0a9fcb0dcb01c3d462d44
m7md-d <wazrahp@gmail.com>
2026-01-31 17:01:24 +0300

    Fix the 10th code.

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -17,8 +17,8 @@ class Square(Rectangle):
         Args:
             size (int): The size of the new Square.
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
-
         self.__size = size
 
     def area(self):
