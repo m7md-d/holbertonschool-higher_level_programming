95e08fc927961b1fb6600c866b2e45497745f2ac
m7md-d <wazrahp@gmail.com>
2026-01-31 14:31:43 +0300

    fix PEP8 validation for the first code.

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,8 +2,10 @@
 """
 Module that defines a function to lookup attributes and methods of an object.
 """
+
+
 def lookup(obj):
     """
-	Returns the list of available attributes and methods of an object.
-	"""
+    Returns the list of available attributes and methods of an object.
+    """
     return dir(obj)
