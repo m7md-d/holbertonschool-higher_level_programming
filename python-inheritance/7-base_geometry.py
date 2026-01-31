b9b55f3fe143c9ddaeeb07f00656d3e40059a409
m7md-d <wazrahp@gmail.com>
2026-01-31 15:26:14 +0300

    Add the 7th code.

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -19,7 +19,7 @@ class BaseGeometry:
         """
         Validates value as an integer greater than 0
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
