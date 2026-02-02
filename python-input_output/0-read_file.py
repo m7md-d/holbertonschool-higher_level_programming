aff8d1f872c8d5528efd6f76975c39f8ec3db91a
Dark-Ghost1 <wazrahm@gmail.com>
2026-02-02 14:19:30 +0300

    Add the first code.

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+0. Read file
+"""
+
+
+def read_file(filename=""):
+    """
+    function that reads a text file and prints it to stdout.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        text = f.read()
+    f.closed
+    print(text)
