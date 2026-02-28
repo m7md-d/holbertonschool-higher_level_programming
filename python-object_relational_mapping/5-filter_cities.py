310b47e3bfece5315820cb95e3ecc79f4f08d908
m7md-d <wazrahp@gmail.com>
2026-02-28 23:22:24 +0300

    Add the 5th code.

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""5. All cities by state"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    query = """
+        SELECT cities.name
+        FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id
+    """
+    cur.execute(query, (sys.argv[4],))
+
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
+
+    cur.close()
+    db.close()
