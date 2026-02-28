294b0b1f3924da2104b8932d587082b9d24938fb
m7md-d <wazrahp@gmail.com>
2026-02-28 23:12:07 +0300

    Fix the 2th code.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     query = "SELECT * FROM states WHERE name = \
-            '{}' ORDER BY id".format(sys.argv[4])
+            BINARY '{}' ORDER BY id".format(sys.argv[4])
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
