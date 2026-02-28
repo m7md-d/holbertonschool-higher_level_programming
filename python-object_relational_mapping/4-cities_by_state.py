2bcdc18af1a41e1c5e1e37a9cd56012c9a26e260
m7md-d <wazrahp@gmail.com>
2026-02-28 23:19:48 +0300

    Fix pycodestyle for the 4th code.

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -7,18 +7,18 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    
+
     query = """
-        SELECT cities.id, cities.name, states.name 
-        FROM cities 
-        INNER JOIN states ON cities.state_id = states.id 
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        INNER JOIN states ON cities.state_id = states.id
         ORDER BY cities.id
     """
     cur.execute(query)
-    
+
     rows = cur.fetchall()
     for row in rows:
         print("({}, '{}', '{}')".format(row[0], row[1], row[2]))
-        
+
     cur.close()
     db.close()
