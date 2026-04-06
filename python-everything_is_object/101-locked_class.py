aa31ab86bf455798107f75cf62b20ef0892008a8
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-04-06 07:44:48 +0300

    style: add missing docstrings to 101-locked_class.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Defines a locked class.
+"""
+
+
 class LockedClass:
-    __slot__ = ["first_name"]
+    """
+    Prevents the user from dynamically creating new instance attributes,
+    except if the attribute is called first_name.
+    """
+    __slots__ = ["first_name"]
